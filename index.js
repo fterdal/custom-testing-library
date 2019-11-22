90be89ae4d5c43ff85a2aeaec8ee099840c8bde3
Finn <finnterdal@gmail.com>
2019-11-22 15:20:21 -0600

    got some indentation

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const { red, green } = require("chalk")
 console.clear()
 
+const logSuccess = (msg, indent = 0) => {
+  process.stdout.write(green("  ".repeat(indent) + msg) + "\n")
+}
+
+const logFailure = (msg, indent = 0) => {
+  process.stdout.write(red("  ".repeat(indent) + msg) + "\n")
+}
+
 function describe(label, testsFn) {
   let registeredTests = []
   let completedTests = []
@@ -10,14 +18,16 @@ function describe(label, testsFn) {
   }
   try {
     testsFn(addTest)
+    console.log(label)
     registeredTests.forEach(test => {
       try {
         test.testFn()
         completedTests.push({ ...test, passing: true })
-        console.log(green(`✅ ${test.id}.) ${test.label}`))
+        // console.log(green(`✅ ${test.id}.) ${test.label}`))
+        logSuccess(`✅ ${test.id}.) ${test.label}`, 1)
       } catch (err) {
         completedTests.push({ ...test, passing: false, err })
-        console.log(red(`🔴 ${test.id}.) ${test.label}`))
+        logFailure(`🔴 ${test.id}.) ${test.label}`, 1)
       }
     })
 
@@ -43,7 +53,7 @@ function it(label, testFn) {
   })
 }
 
-describe("some tests go here", function() {
+describe("Some Tests Go Here", function() {
   it("passing test", function() {})
   it("failing test", function() {
     throw new Error("Something went wrong")
